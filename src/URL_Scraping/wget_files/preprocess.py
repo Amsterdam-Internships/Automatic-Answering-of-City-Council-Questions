902a005fd6b9799aa487ceb25d583024f92cde7e
Natali Peeva <natali.peeva@gmail.com>
2023-04-19 09:36:54 +0200

    preprocess.py - includesplit paragraphs

--- src/URL_Scraping/wget_files/preprocess.py
+++ src/URL_Scraping/wget_files/preprocess.py
@@ -1,4 +1,5 @@
 import os
+from bs4 import BeautifulSoup
 
 def read_folders(root_path):
     """
@@ -14,3 +15,16 @@ def read_folders(root_path):
                 with open(file_path, 'r',  encoding='ISO-8859-1') as file:
                     html_contents.append(file.read())
     return html_contents
+
+
+def split_paragraphs(html_content):
+    """
+    Input: HTML content 
+    Output: A List of type(list) of HTML textual content splitted in paragraphs
+    """
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # find all <p> tags and extract their text contents
+    paragraphs = [p.get_text() for p in soup.find_all('p')]
+
+    return paragraphs
