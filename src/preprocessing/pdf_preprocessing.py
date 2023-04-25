388806c0fe0d6fcc3c28fc8bbc9d2adacc0edb23
Natali Peeva <natali.peeva@gmail.com>
2023-04-25 12:26:51 +0200

    Create pdf_preprocessing.py

---
+++ src/preprocessing/pdf_preprocessing.py
@@ -0,0 +1,19 @@
+
+import PyPDF2
+import requests
+
+def get_pdf_text(pdf_url):
+    """
+    Input: PDF URL
+    Output: PDF textual content
+    """
+    pdf = requests.get(pdf_url)
+    pdf_content = pdf.content 
+    pdf_text = io.BytesIO(pdf_content)
+    pdf_reader = PyPDF2.PdfReader(pdf_text)
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+    
+    return text
