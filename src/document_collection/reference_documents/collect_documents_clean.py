6db10880060d8b824f00992b2118bb0784308731
Natali Peeva <natali.peeva@gmail.com>
2023-05-15 15:47:02 +0200

    Fix apply_url_fetching function in collect_documents_clean.py

--- src/document_collection/reference_documents/collect_documents_clean.py
+++ src/document_collection/reference_documents/collect_documents_clean.py
@@ -1,16 +1,19 @@
 import sys
 import os
-src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Draft/', 'src')
+src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Automatic-Answering-of-City-Council-Questions/', 'src')
 sys.path.append(src_dir)
 from dataset.dataset import ReferenceURL
 
 
-def apply_url_fetching(urls):
+def apply_url_fetching(urls, timeout):
     fetched_urls = []
     for url in urls:
         url = ReferenceURL(url)
         try:
-            fetched_urls.append(url.fetch_url()) # set timeout
+            url.fetch_url(timeout)
+            fetched_urls.append(url) # set timeout
         except Exception as e:
-            fetched_urls.append(url, e) ################ check it
+            fetched_urls.append((url, e)) ################ check it
             ### store to check exceptions
+    
+    return fetched_urls
