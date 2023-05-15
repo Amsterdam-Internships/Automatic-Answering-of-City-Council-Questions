591506d1eb2e717145f97d81853ad33aea1b9e34
Natali Peeva <natali.peeva@gmail.com>
2023-05-15 10:06:46 +0200

    Delete wget files folder

--- src/URL_Scraping/wget_files/wget.sh
+++
@@ -1,7 +0,0 @@
-wget --recursive \\
---no-clobber \\
---page-requisites \\
---convert-links \\
---adjust-extension \\
-accept-regex 'https://www.amsterdam.nl/afval-en-hergebruik/.*' \\
-https://www.amsterdam.nl/afval-en-hergebruik/
