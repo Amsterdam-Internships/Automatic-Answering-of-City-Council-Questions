413f70239e54aa130c59e3719ea0666e32e06930
Natali Peeva <natali.peeva@gmail.com>
2023-05-15 04:06:56 +0200

    Creatte collect_documents_clean.py

---
+++ src/document_collection/supporting_documents/collect_documents_clean.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import random
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+import os
+import sys
+src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Draft/', 'src')
+sys.path.append(src_dir)
+from dataset.dataset import ReferenceURL
+
+# amsterdam.nl
+def collect_subpages(base_url, paths):
+    collected_urls = []
+
+    base_url = ReferenceURL(base_url)
+    base_url.fetch_url(random.uniform(1, 3))
+    collected_urls.append(base_url)
+
+    links = base_url.html_content.find_all("a", href=lambda href: href)
+
+    for link in links:
+        if any(path in link["href"] for path in paths):
+            linked_url = ReferenceURL(urljoin(base_url.url, link["href"]))
+            linked_url.fetch_url(random.uniform(2, 4))
+            collected_urls.append(linked_url)
+            # Recursively follow links that contain paths
+            if any(path in linked_url.url for path in paths):
+                subpages = collect_subpages(linked_url.url, paths)
+                collected_urls.extend(subpages)
+
+    return collected_urls
