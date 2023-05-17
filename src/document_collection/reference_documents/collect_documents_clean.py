ee6692b71bdfd6950c12321029e3d7bbf470fa73
Natali Peeva <natali.peeva@gmail.com>
2023-05-17 09:56:04 +0200

    Add saving functions

--- src/document_collection/reference_documents/collect_documents_clean.py
+++ src/document_collection/reference_documents/collect_documents_clean.py
@@ -4,7 +4,46 @@ src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Automatic-Answering-
 sys.path.append(src_dir)
 from dataset.dataset import ReferenceURL
 import time
+import pickle 
+import pandas as pd 
 
+########################## collection ###################
+
+
+def collect_all(data, timeout, waiting_time):
+    for sample in data:
+        sample.collect_urls(timeout, waiting_time)
+
+
+
+def save_data_question_answer(data):
+    data_tuples = []
+    #self.urls = urls
+    #self.collected_urls = None
+    for sample in data:
+        urls = []
+        for url in sample.collected_urls:
+            urls.append(url.url) 
+        
+        data_tuples.append((sample.year, sample.month, sample.question, sample.answer, sample.document, urls))
+
+    with open ('data_updated_urls.pickle', 'wb') as f:
+        pickle.dump(data_tuples, f)
+
+
+def save_collected_urls(data):
+    url_tuples = []
+
+    for sample in data:
+        for url in sample.collected_urls:
+            url_tuples.append((url.url, url.content, url.text, url.exception))
+    
+    df = pd.DataFrame(url_tuples, columns=['URL', 'Content', 'Textual_Content', 'Exception'])
+
+    df.to_csv('urls_collected_16May.csv')
+
+
+###################### older function: #################
 def apply_url_fetching(urls, timeout, waiting_time=None):
     fetched_urls = []
     for url in urls:
@@ -19,12 +58,3 @@ def apply_url_fetching(urls, timeout, waiting_time=None):
         time.sleep(waiting_time) # waiting time before next request
     
     return fetched_urls
-
-
-
-########################## collection ###################
-
-
-def collect_all(data):
-    for sample in data:
-        sample.collect_urls(timeout=5, waiting_time=2)
