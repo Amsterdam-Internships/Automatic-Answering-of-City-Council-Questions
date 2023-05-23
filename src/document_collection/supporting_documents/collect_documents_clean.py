e070b3f80c287488f04437d1a15dae7012aad3ed
Natali Peeva <natali.peeva@gmail.com>
2023-05-23 10:54:31 +0200

    Update collect_documents_clean.py

--- src/document_collection/supporting_documents/collect_documents_clean.py
+++ src/document_collection/supporting_documents/collect_documents_clean.py
@@ -10,7 +10,10 @@ src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Draft/', 'src')
 sys.path.append(src_dir)
 from dataset.dataset import ReferenceURL
 
-# amsterdam.nl
+###################################################################################################
+##################################### amsterdam.nl ################################################ 
+###################################################################################################
+
 def collect_subpages(base_url, paths):
     collected_urls = []
 
@@ -31,3 +34,18 @@ def collect_subpages(base_url, paths):
                 collected_urls.extend(subpages)
 
     return collected_urls
+
+paths = ['afval-en-hergebruik', 'afval-hergebruik', 'belastingen-heffingen', 'bestuur-en-organisatie',
+         'bestuur-organisatie', 'projecten', 'burgerzaken', 'diversiteit', 'kunst-cultuur', 
+         'ondernemen', 'onderwijs-jeugd', 'parkeren', 'sport', 'stadsdelen', 'subsidies', 
+         'verkeer-vervoer', 'toerisme-vrije-tijd', 'werk-inkomen', 'wonen-leefomgeving', 
+         'zorg-ondersteuning']
+
+
+base_url = 'https://www.amsterdam.nl/'
+
+def collect_amsterdam(base_url = base_url, paths = paths):
+    collect_subpages(base_url, paths)
+
+###################################################################################################
+
