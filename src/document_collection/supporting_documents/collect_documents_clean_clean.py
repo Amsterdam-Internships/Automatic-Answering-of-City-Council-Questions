75dde210a17440febb79920043510a4c9f98e51f
Natali Peeva <natali.peeva@gmail.com>
2023-05-31 08:42:27 +0200

    Add possibility for keyboard interruption in URL collection

--- src/document_collection/supporting_documents/collect_documents_clean_clean.py
+++ src/document_collection/supporting_documents/collect_documents_clean_clean.py
@@ -18,7 +18,7 @@ def collect_subpages(base_url):
     visited_urls = set()
     collection = []
     urls_to_visit = [base_url]
-
+    
     while urls_to_visit:
         url = ReferenceURL(url = urls_to_visit.pop(0))
         
@@ -54,11 +54,72 @@ def collect_subpages(base_url):
                             # Add the URL to the list of URLs to visit
                             urls_to_visit.append(absolute_url.url)
 
-
+        except KeyboardInterrupt:
+                # Handle interruption
+                print("Execution interrupted!")
+                return collection
         except:
             pass
     
     return collection
+
+def collect_subpages2(base_url):
+    """
+    Input: base url like https://amsterdam.nl/parkeren
+    Output: a scraped collection of all nested urls under the base one
+    """
+    visited_urls = set()
+    collection = []
+    urls_to_visit = [base_url]
+    
+    try:
+        while urls_to_visit:
+            url = ReferenceURL(url = urls_to_visit.pop(0))
+            
+            # Skip if already visited
+            if url.url in visited_urls:
+                continue
+
+            # Add to visited URLs
+            visited_urls.add(url.url)
+            url.fetch_url(timeout=5)
+
+            # Add check collection???
+
+            time.sleep(random.uniform(1, 3))
+            try:
+                url.fetch_url()
+                collection.append(url)
+                # Extract all anchor tags
+                if url.content is None:
+                    pass
+                else:
+                    for anchor_tag in url.content.find_all('a'):
+                        href = anchor_tag.get('href')
+                        if href:
+                            # Construct absolute URL
+                            absolute_url = urljoin(url.url, href)
+                            absolute_url = ReferenceURL(absolute_url)
+                        
+                            # Check if the URL contains the base URL and exclude specified patterns
+                            if base_url in absolute_url.url and \
+                                    not any(pattern in absolute_url.url for pattern in ['#Content', '#mainmenu', '#megamenu', '?mode=hide', '#PagCls', 'tel:', '?print=true']):
+                                
+                                # Add the URL to the list of URLs to visit
+                                urls_to_visit.append(absolute_url.url)
+
+            except KeyboardInterrupt:
+                # Handle interruption
+                print("Execution interrupted!")
+                break
+            except:
+                pass
+    
+    except:
+        pass
+    
+    return collection
+
 
 
 
