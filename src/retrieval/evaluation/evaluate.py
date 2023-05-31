1aa253efa0edec74fa44431e851d07cd88ffc730
Natali Peeva <natali.peeva@gmail.com>
2023-05-31 09:28:09 +0200

    Remove unnecessary functions

--- src/retrieval/evaluation/evaluate.py
+++ src/retrieval/evaluation/evaluate.py
@@ -103,42 +103,4 @@ def calculate_average_recall(recall_results):
     return average_recall
 
 
-#########################################################################################
 
-def write_trec_run(results, outfn, tag="trecrun"):
-    with open(outfn, "wt") as outf:
-        qids = sorted(results.keys())
-        for qid in qids:
-            rank = 1
-            for docid, score in sorted(results[qid], key=lambda x: x[1], reverse=True):
-                print(f"{qid} Q0 {docid} {rank} {score} {tag}", file=outf)
-                rank += 1
-
-
-def evaluate_search_fn(method_name, search_fn, metric_fns, dh, queries, qrels, index_set=None):
-    # build a dict query_id -> query
-    queries_by_id = dict((q[0], q[1]) for q in queries)
-
-    metrics = {}
-    for metric, metric_fn in metric_fns:
-        metrics[metric] = np.zeros(len(qrels), dtype=np.float32)
-
-    q_results = {}
-    for i, (query_id, relevant_docs) in enumerate(qrels.items()):
-        query = queries_by_id[query_id]
-        if index_set:
-            results = search_fn(query, dh, index_set)
-        else:
-            results = search_fn(query, dh)
-        
-        q_results[query_id] = results
-        for metric, metric_fn in metric_fns:
-            metrics[metric][i] = metric_fn(results, relevant_docs)
-
-    write_trec_run(q_results, f'{method_name}_{index_set}.trec')
-
-    final_dict = {}
-    for metric, metric_vals in metrics.items():
-        final_dict[metric] = metric_vals.mean()
-
-    return final_dict
