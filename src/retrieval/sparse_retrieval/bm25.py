74c7704763772ea45b0448925b3eab3adbd59ade
Natali Peeva <natali.peeva@gmail.com>
2023-05-31 08:42:39 +0200

    Update bm25.py

--- src/retrieval/sparse_retrieval/bm25.py
+++ src/retrieval/sparse_retrieval/bm25.py
@@ -1,59 +1,71 @@
 from elasticsearch import Elasticsearch
 
-def bm25_elasticsearch(passages, queries):
-    es = Elasticsearch(hosts=["http://localhost:9200"])
 
-    # Index configuration
-    index_name = "passages_index"
+def set_index(mappings, es_client, collected):
 
-    # Check if the index already exists
-    if not es.indices.exists(index=index_name):
-        # Create the index with BM25 similarity
-        es.indices.create(index=index_name, body={"settings": {"index": {"similarity": {"default": {"type": "BM25"}}}}})
-    
-        # Index the passages
-        for i, passage in enumerate(passages):
-            passage_id = f"{passage['id']}_{i}"  # Append a unique identifier to the ID
-            es.index(index=index_name, id=passage_id, body={
-                #"original_id": passage['id'],
-                "content": passage['content']
-            })
+    es_client.indices.delete(index="qa_attempt1", ignore=[400, 404])
+    es_client.indices.create(index="qa_attempt1", mappings=mappings)
 
-        # Refresh the index to make the documents searchable
-        es.indices.refresh(index=index_name)
-
-    results = []
-    # Perform the search for each query
-    for query in queries:
-        # Formulate the search query
-        search_body = {
-            "query": {
-                "match": {
-                    "content": query
-                }
-            }
+    for i, row in collected.iterrows():
+        doc = {
+            "url": row["URL"],
+            "text": str(row["Textual_Content"])
         }
+                
+        es_client.index(index="qa_attempt1", id=i, document=doc)
 
-        # Execute the search
-        response = es.search(index=index_name, body=search_body)
+    return es_client
 
-        # Store the search results in a list of dictionaries
-        for hit in response["hits"]["hits"]:
-            score = hit["_score"]
-            passage_id = hit["_id"]
-            passage = hit["_source"]
-            original_id = passage["original_id"]  # Retrieve the original passage ID
-            results.append({"id": passage_id, "content": passage["content"], "score": score}) # "original_id": original_id
 
+
+def query_es_index(question, es_client, index_name="qa_attempt1", n_results=10, min_length=20):
+    """
+    Perform a search query on an Elasticsearch index based on a provided question.
+    """
+    q = question.lower()
+    banned = [] #["how", "why", "what", "where", "which", "do", "does", "is", "?", "eli5", "eli5:"]
+    #q = " ".join([w for w in q.split() if w not in banned])
+    response = es_client.search(
+        index=index_name,
+        body={
+            "query": {
+                "multi_match": {
+                    "query": q,
+                    "fields": ["url", "text"],
+                    "type": "cross_fields",
+                }
+            },
+            "size": 2 * n_results,
+        },
+    )
+    hits = response["hits"]["hits"]
+    support_doc = "<P> " + hits[0]["_source"]["text"] if hits else ""
+    res_list = [
+        {
+            k: hit["_source"][k]
+            for k in hit["_source"]
+            if k != "passage_text"
+        }
+        for hit in hits
+    ]
+    for r, hit in zip(res_list, hits):
+        r["passage_id"] = hit["_id"]
+        r["score"] = hit["_score"]
+        r["passage_text"] = hit["_source"]["text"]
+    res_list = [
+        res
+        for res in res_list
+        if len(res["passage_text"].split()) > min_length
+    ][:n_results]
+    return support_doc, res_list
+
+def get_result_tuples(es_client, questions, index_name='qa_attempt1'):
+
+    results = {}  # To store the results for all questions
+
+    for question in questions:
+        doc, res_list = query_es_index(question, es_client, index_name=index_name)
+        results[question] = (doc, res_list)  # it took 9 seconds to finish 
+    
     return results
 
-
-
-
-
-# Print the stored results
-"""for result in results:
-    print(f"Passage ID: {result['id']}")
-    print(f"Original ID: {result['original_id']}")
-    print(f"Content: {result['content']}")
-    print(f"Score: {result['score']}\n") """
