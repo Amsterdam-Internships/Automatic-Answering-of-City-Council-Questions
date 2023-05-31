697703b2ac5e2ebc3b76e1725922849008214d6e
Natali Peeva <natali.peeva@gmail.com>
2023-05-31 09:29:22 +0200

    Remove unnecessary function

--- src/document_collection/reference_documents/collect_documents_clean.py
+++ src/document_collection/reference_documents/collect_documents_clean.py
@@ -46,22 +46,3 @@ def save_collected_urls(data):
     df = pd.DataFrame(url_tuples, columns=['URL', 'Content', 'Textual_Content', 'Exception'])
 
     df.to_csv('reference_urls_collected.csv')
-
-
-
-
-###################### older function: #################
-def apply_url_fetching(urls, timeout, waiting_time=None):
-    fetched_urls = []
-    for url in urls:
-        url = ReferenceURL(url)
-        try:
-            url.fetch_url(timeout)
-            fetched_urls.append(url)
-        except Exception as e:
-            fetched_urls.append((url, e)) ################ check it
-            ### store to check exceptions
-        
-        time.sleep(waiting_time) # waiting time before next request
-    
-    return fetched_urls
