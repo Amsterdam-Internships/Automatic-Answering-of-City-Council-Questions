12d81e20004c3b0a81bf96efd64be025e560c6a3
Natali Peeva <natali.peeva@gmail.com>
2023-06-08 10:22:09 +0200

    Update paths in reference collection script

--- src/document_collection/reference_documents/collect_documents_clean.py
+++ src/document_collection/reference_documents/collect_documents_clean.py
@@ -1,7 +1,13 @@
 import sys
 import os
-src_dir = os.path.join('/Users/natalipeeva/Documents/GitHub/Automatic-Answering-of-City-Council-Questions/', 'src')
+
+# absolute path of the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# add the relative path to the source directory
+src_dir = os.path.join(current_dir, 'src')
 sys.path.append(src_dir)
+
 from dataset.dataset import ReferenceURL
 import time
 import pickle 
